8719e9646516b8f76fdac53cd3e1b9a404cdfd41
Nikki Petersen <piknik@me.com>
2020-09-12 12:03:29 -0600

    sizing and background image

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    Cookie component
-
-    <button> 🍪 Got it!! </button>
+    <div id="container" class="box">
+      <div class="content">We use cookies to ensure you get the best experience on our website.</div>
+      <a href="#">Our privacy policy</a>
+      <br>
+      <button class="button is-warning"> 🍪 NomNomNom!! </button>
+    </div>
   </div>
 </template>
 
@@ -15,11 +18,35 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+#container {
+  margin: 0 auto;
+  background-image: url("./assets/CM1.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 500px;
+  height: auto;
+  background-color: #13B0EF;
+}
+
+a {
+  color: white;
+  border-bottom: 6px solid lightseagreen;
+  margin: 10px;
+}
+
+.content {
+  color: white;
+  width: 50%;
+  margin: 10px;
+}
+
+button {
+  margin: 15px 10px 5px;
 }
 </style>
